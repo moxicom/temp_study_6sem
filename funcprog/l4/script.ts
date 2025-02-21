426bde87178ad87526d571203d4c1c3ce2beee8f
Alexander Markov <markovalex2004@gmail.com>
2025-02-21 23:02:53 +0300

    ddd comment

--- funcprog/l4/script.ts
+++ funcprog/l4/script.ts
@@ -1,3 +1,5 @@
+// tsc script.ts
+
 const add = (a: number, b: number): number => a + b;
 const subtract = (a: number, b: number): number => a - b;
 const multiply = (a: number, b: number): number => a * b;
